213358ac80c0a6022add8f9032f149686bca8318
Diego Luis <diego_bezerra95@hotmail.com>
2024-03-05 23:16:04 -0300

    Implementacao prometheus mais grafana para acompanhamento de metricas

--- src/main/java/com/transaction/infra/util/GenerateAccountNumber.java
+++ src/main/java/com/transaction/infra/util/GenerateAccountNumber.java
@@ -7,11 +7,12 @@ import java.util.Random;
 @UtilityClass
 public class GenerateAccountNumber {
 
+    private static final Random RANDOM = new Random();
+
     public static String generateAccountNumber() {
-        var random = new Random();
         var numberAccount = new StringBuilder();
         for (int i = 0; i < 6; i++) {
-            numberAccount.append(random.nextInt(10));
+            numberAccount.append(RANDOM.nextInt(10));
         }
         return numberAccount.toString();
     }
